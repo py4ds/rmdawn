6cc603962f3dc2f6b574e41d472dea71493d9af7
Martin Skarzynski <marskar@gmail.com>
2019-05-10 15:15:50 -0400

    Bump version from 0.0.8

---
+++ test_1.py
@@ -0,0 +1,59 @@
+#+ python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+import setuptools
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = ['Click>=6.0', ]
+
+setup_requirements = ['pytest-runner', ]
+
+test_requirements = ['pytest', ]
+
+setuptools.setup(
+    author="Martin Skarzynski",
+    author_email='marskar@gmail.com',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    description="Extract code, markdown, and yaml files from R markdown.",
+    entry_points={
+        'console_scripts': [
+            'rmdusk=cli.rmdusk_cli:rmdusk_cli',
+            'rmdawn=cli.rmdawn_cli:rmdawn_cli',
+        ],
+    },
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    long_description_content_type='text/x-rst',
+    include_package_data=True,
+    keywords='rmdawn',
+    name='rmdawn',
+    package_dir={"": "src"},
+    packages=setuptools.find_packages('src'),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/marskar/rmdawn',
+    version='0.0.8',
+    zip_safe=False,
+)
