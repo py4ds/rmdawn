11993513c3da4cc756b67efa828b5e7f5c962446
Martin Skarzynski <marskar@gmail.com>
2019-05-10 15:15:52 -0400

    Bump version: 0.0.8 → 0.0.9

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setuptools.setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/marskar/rmdawn',
-    version='0.0.8',
+    version='0.0.9',
     zip_safe=False,
 )
