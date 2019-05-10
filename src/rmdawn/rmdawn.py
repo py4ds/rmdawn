b91f791e0f235f7af7aa1379df60dbc6d52ec2e3
Martin Skarzynski <marskar@gmail.com>
2019-05-10 12:52:10 -0400

    write main functions

--- src/rmdawn/rmdawn.py
+++ src/rmdawn/rmdawn.py
@@ -8,12 +8,12 @@ from pathlib import Path
 def rmdawn(filenames: List[str]) -> str:
     """Create an R markdown file from markdown and code files."""
     return "\n\n".join([
-        '```{r}\n'+Path(name).read_text()+'\n```'
-        if name.endswith('.R')
-        else '---\n'+Path(name).read_text()+'\n---'
-        if name.endswith(('.yaml', 'yml'))
-        else '```{python}\n'+Path(name).read_text()+'\n```'
-        if name.endswith('.py')
+        "---\n" + Path(name).read_text() + "\n---"
+        if name.endswith((".yaml", "yml"))
+        else "```{python}\n" + Path(name).read_text() + "\n```"
+        if name.endswith(".py")
+        else "```{r}\n" + Path(name).read_text() + "\n```"
+        if name.endswith(".R")
         else Path(name).read_text()
         for name in filenames
     ])
--- src/rmdawn/rmdusk.py
+++ src/rmdawn/rmdusk.py
@@ -1,3 +1,41 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Extract YAML, code, and markdown files from an R markdown file."""
+import re
+from pathlib import Path
+
+
+def extract_between(string, start, end):
+    return string[string.find(start) + len(start): string.find(end)]
+
+
+def extract_after(string, start):
+    return string[string.find(start) + len(start):]
+
+
+def rmdusk(filename: str) -> str:
+    """Extract YAML, code, and markdown files from an R markdown file."""
+
+    rmd_path = Path(filename)
+    rmd_text = rmd_path.read_text()
+
+    Path(rmd_path.with_suffix(".yml")).write_text(
+        extract_between(rmd_text, start="^---\n", end="\n---\n")
+    )
+
+    code_and_text = re.sub(
+        r"```{(.*?)}",
+        r"```#+ \1",
+        extract_after(rmd_text, "\n---\n"),
+        flags=re.DOTALL
+    ).split("```")
+
+    for n, string in enumerate(code_and_text):
+        if string.startswith("#+ python"):
+            Path(rmd_path.stem + f"_{n}.py").write_text(string.strip())
+        if string.startswith("#+ r"):
+            Path(rmd_path.stem + f"_{n}.R").write_text(string.strip())
+        else:
+            Path(rmd_path.stem + f"_{n}.md").write_text(string.strip())
+
+
+rmdusk("example.Rmd")
