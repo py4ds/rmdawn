9466255ef39ee5f1071a230351641a762f558ca1
Martin Skarzynski <marskar@gmail.com>
2019-05-16 09:13:50 -0400

    add rpy2 to install_requires in setup.py

--- setup.py
+++ setup.py
@@ -11,9 +11,9 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=6.0', ]
+requirements = ['Click>=6.0', "rpy2"]
 
-setup_requirements = ['pytest-runner', "rpy2"]
+setup_requirements = ['pytest-runner', ]
 
 test_requirements = ['pytest']
 
