ada645c503c936ff4ab673844e6815ac15deb7ee
Martin Skarzynski <marskar@gmail.com>
2019-05-29 12:21:02 -0400

    try to fix Optional and None type hints

--- src/cli/rmdtor_cli.py
+++ src/cli/rmdtor_cli.py
@@ -9,10 +9,11 @@ from rmdawn.rmdtor import rmdtor
 @click.command()
 @click.argument("in_file")
 @click.option("-o", "--out_file")
-def rmdtor_cli(in_file: str, out_file: Optional[str] = None):
+def rmdtor_cli(in_file: str, out_file: Optional[str] = None) -> Optional[str]:
     """Convert an R markdown file into an R script.
 
     :param in_file: The name of the input R markdown file.
     :param out_file: The name of the output R script.
     """
     rmdtor(in_file, out_file=out_file) if out_file else rmdtor(in_file)
+    return f"Created {out_file}." if out_file else None
